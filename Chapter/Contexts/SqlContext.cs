1c177d60586d70b9f3213b5c442be1f6ed8bce99
Arthur <arthurdlima99@gmail.com>
2022-09-26 18:23:51 -0300

    swagger works

---
+++ Chapter/Contexts/SqlContext.cs
@@ -0,0 +1,29 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+        }
+        public SqlContext(DbContextOptions<SqlContext> options)
+        : base(options)
+        {
+        }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+            optionsBuilder.UseSqlServer("Data Source = DESKTOP-UOAN0LR\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            //optionsBuilder.UseSqlServer("Data Source = DESKTOP-UOAN0LR\\SQLEXPRESS; initial catalog = Chapter; User ID = aa; Password = 1232");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ Chapter/Repositories/LivroRepository.cs
@@ -0,0 +1,25 @@
+﻿using Chapter.Contexts;
+using Chapter.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+
+namespace Chapter.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SqlContext _context;
+        public LivroRepository(SqlContext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        
+        }
+
+
+    }
+}
